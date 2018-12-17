827582519fbcef25f19144445012043ef7b45299
Pedro Duarte Carvalho <dudu38carvalho@gmail.com>
2018-12-17 11:56:35 +0000

    commit actually

--- WebContent/src/rmiserver/MulticastServer.java
+++ WebContent/src/rmiserver/MulticastServer.java
@@ -2039,7 +2039,7 @@ public class MulticastServer extends Thread implements Serializable {
 
     private Connection initConnection() {
         try{
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BD/SD","postgres", "fabiogc1998");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BD/SD","postgres", "1717");
         } catch (SQLException e) {
             e.printStackTrace();
         }
